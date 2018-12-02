a8996e051914c60a3a8de7f27bb193fd8c157e73
madara.freimane <madara.freimane@testdevlab.com>
2018-12-02 17:25:06 +0200

    homework1

--- src/main/java/JavaCourse.java
+++ src/main/java/JavaCourse.java
@@ -1,5 +1,17 @@
 package main.java;
 
 public class JavaCourse {
+    public static void main(String[] args){
+        String firstStringVariable = "I'm starting to learn Java basics!";
+        String textValueFromOtherMethod = getSomeText();
 
+
+        System.out.println("Hello TDL");
+        System.out.println(firstStringVariable);
+        System.out.println(textValueFromOtherMethod);
+    }
+
+    private static String getSomeText() {
+        return "So far so good!";
+    }
 }
