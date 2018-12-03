a7eaed924c3f0caee8cc66de4c278ea55102bd77
madara.freimane <madara.freimane@testdevlab.com>
2018-12-03 21:16:28 +0200

    Homework-1.1

--- src/main/java/simplytext/PrintNumbers.java
+++ src/main/java/simplytext/PrintNumbers.java
@@ -4,27 +4,25 @@ import static main.java.printer.PrinterForNumbers.printTextLine1;
 
 public class PrintNumbers {
 
-        public void displayNumbers() {
-            int textValueFromOtherMethod3 = getSomeNumber();
-            double textValueFromOtherMethod4 = getDoubleNumber();
-            Integer textValueFromOtherMethod5 = getIntegerNumber();
+    public void displayNumbers() {
+        int textValueFromOtherMethod3 = getSomeNumber();
+        double textValueFromOtherMethod4 = getDoubleNumber();
+        Integer textValueFromOtherMethod5 = getIntegerNumber();
 
 
-
-            printTextLine1(textValueFromOtherMethod3);
-            printTextLine1(textValueFromOtherMethod4);
-            printTextLine1(textValueFromOtherMethod5);
-
+        printTextLine1(textValueFromOtherMethod3);
+        printTextLine1(textValueFromOtherMethod4);
+        printTextLine1(textValueFromOtherMethod5);
 
 
-        }
+    }
 
-        private int getSomeNumber() {
-            return 777;
-        }
+    private int getSomeNumber() {
+        return 777;
+    }
 
     private double getDoubleNumber() {
-            return 7.1;
+        return 7.1;
     }
 
     private static Integer getIntegerNumber() {
@@ -32,4 +30,4 @@ public class PrintNumbers {
     }
 
 
-    }
+}
