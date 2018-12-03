8a7c778404d9fb6f1f7d71628f552154e56a5471
madara.freimane <madara.freimane@testdevlab.com>
2018-12-03 23:50:58 +0200

    Task2

---
+++ src/main/java/human/Teacher.java
@@ -0,0 +1,15 @@
+package main.java.human;
+
+public class Teacher extends Human {
+
+    private double salary;
+
+    public Teacher(String firstName, String lastName, int age, double salary) {
+        super(firstName, lastName, age);
+        this.salary = salary;
+    }
+
+    public String getSalary() {
+        return String.valueOf(this.salary);
+    }
+}
