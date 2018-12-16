7dfb88c4191ea71c32d0ccf38f9c5c4eb6b18dde
madara.freimane <madara.freimane@testdevlab.com>
2018-12-16 18:36:07 +0200

    Lecture7

--- src/main/java/MasterMethodsExecutorAndObjectCreator.java
+++ src/main/java/MasterMethodsExecutorAndObjectCreator.java
@@ -3,6 +3,12 @@ package main.java;
 import main.java.human.Human;
 import main.java.human.Student;
 import main.java.simplytext.TextHolderAndDyplayer;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import static main.java.printer.Printer.*;
 import static main.java.projectConstants.SomeConstants.STUDENT_NAME;
 
@@ -11,8 +17,12 @@ public class MasterMethodsExecutorAndObjectCreator {
 //        callMethodWhichWillPrintSomeText();
 //        createHuman();
 //        createStudent();
-        operationsWithIfs();
-
+//        operationsWithIfs();
+//        exampleWithSwitch();
+//        exampleWithForLoop();
+//        exampleWithForEachLoop();
+//        exampleOfArray();
+        exampleWithMap();
     }
 
     private void callMethodWhichWillPrintSomeText() {
@@ -87,5 +97,61 @@ public class MasterMethodsExecutorAndObjectCreator {
         }
 
     }
+
+    private void exampleWithSwitch() {
+        String switchExample = "OOP";
+        switch (switchExample) {
+            case "wrong":
+                System.out.println("first");
+                break;
+            case "OOP":
+                System.out.println("second");
+                break;
+            default:
+                System.out.println("This is default");
+                break;
+        }
+    }
+
+    private void exampleWithForLoop() {
+        for(int times = 0; times <= 5; times++) {
+            System.out.println("Going round and round " + times);
+        }
+    }
+
+    private void exampleWithForEachLoop() {
+        List<String> forEachExampleValues = new ArrayList<>();
+        forEachExampleValues.add("December");
+        forEachExampleValues.add("January");
+        forEachExampleValues.add("February");
+
+        for(String temp : forEachExampleValues) {
+            System.out.println(temp);
+        }
+    }
+
+    private void exampleOfArray() {
+        String[] arrayExample = new String[3];
+        arrayExample[0] = "December1";
+        arrayExample[1] = "January1";
+        arrayExample[2] = "February1";
+
+        for(String temp : arrayExample) {
+            System.out.println(temp);
+        }
+    }
+
+    private void exampleWithMap() {
+        Map<Integer, String> mapValues = new HashMap<>();
+        mapValues.put(111, "January");
+        mapValues.put(222, "February");
+        mapValues.put(333, "March");
+
+        for(String monthTemp : mapValues.values()) {
+            System.out.println(monthTemp);
+        }
+
+    }
+
 
 }
